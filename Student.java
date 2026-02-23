c10d71f96b37284ffeb6b5869869396fac5a3a00
Jeffreyturqueza <88072876+Jeffreyturqueza@users.noreply.github.com>
2026-02-24 04:03:25 +0800

    Add getter methods to Student class

--- Student.java
+++ Student.java
@@ -11,3 +11,14 @@ public class Student {
     private String email;
 
     public Student() {}
+
+    public int getStudentID() { return studentID; }
+    public String getFirstName() { return firstName; }
+    public String getMiddleName() { return middleName; }
+    public String getLastName() { return lastName; }
+    public int getAge() { return age; }
+    public String getGender() { return gender; }
+    public String getCourse() { return course; }
+    public int getYearLevel() { return yearLevel; }
+    public String getSection() { return section; }
+    public String getEmail() { return email; }
